9fb61f8e5ae6b0d28085e514b5cdce6ca0c99502
Lukasz Przewlocki <luxxart@gmail.com>
2020-03-06 09:15:29 +0000

    configuration via API enabled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     const initialRCFInterestRate = 3;
     const initialBLInterestRate = 3;
     const bLInitialFeePercent = 10;
+    const configUrl = 'http://www.mocky.io/v2/5d4aa9e93300006f000f5ea9'
 
     const [rcfRates, setRcfRates] = useState([]);
     const [blRates, setBlRates] = useState([]);
@@ -18,6 +20,30 @@ function App() {
     const [duration, setDuration] = useState(initialDuration);
     const [rCFInterestRate, setRCFInterestRate] = useState(initialRCFInterestRate);
     const [bLInterestRate, setBLInterestRate] = useState(initialBLInterestRate);
+
+    const [configLoaded, setConfigLoaded] = useState(false);
+
+    const [rcfConfig, setRcfConfig] = useState({})
+    const [blConfig, setBlConfig] = useState({})
+
+    const [rcfValid, setRcfValid] = useState(false);
+    const [blValid, setBlValid] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const config = await axios.get(configUrl);
+                const configData = config.data;
+
+                setRcfConfig(configData.revolving_credit_facility);
+                setBlConfig(configData.business_loan);
+                setConfigLoaded(true)
+            }
+            catch (err) {
+                console.error("error:", err);
+            }
+        })()
+    }, []);
 
     useEffect(() => {
         const bLInitialFee = amount / bLInitialFeePercent;
@@ -44,9 +70,17 @@ function App() {
             }
             return rates;
         };
+        const isProductValid = (productConfig) => {
+            return amount >= productConfig.amount_min
+                && amount <= productConfig.amount_max
+                && duration >= productConfig.duration_min
+                && duration <= productConfig.duration_max;
+        }
         setRcfRates(calculateRates(rCFInterestRate, 0));
         setBlRates(calculateRates(bLInterestRate, bLInitialFee));
-    }, [amount, duration, rCFInterestRate, bLInterestRate]);
+        setRcfValid(isProductValid(rcfConfig));
+        setBlValid(isProductValid(blConfig));
+    }, [amount, duration, rCFInterestRate, bLInterestRate, rcfConfig, blConfig]);
 
     const onDurationChange = (ev) => {
         setDuration(ev.target.value);
@@ -64,79 +98,114 @@ function App() {
         setBLInterestRate(value);
     };
 
+
+    const renderForm = () => {
+        return (
+            <form>
+                <div>
+                    <label>
+                        Amount requested
+                        <input
+                            type="number"
+                            value={amount}
+                            min={minAmount}
+                            max={maxAmount}
+                            onChange={onAmountChange}
+                        />
+                        (£)
+                    </label>
+                </div>
+                <div>
+                    Duration
+                    <input
+                        type="number"
+                        value={duration}
+                        min={minDuration}
+                        max={maxDuration}
+                        onChange={onDurationChange}
+                    />
+                    (months)
+                </div>
+            </form>
+        )
+    };
+
+    const renderTables = () => {
+        return (
+            <div style={{display: 'flex', padding: 44}}>
+                <span style={{width: '50%'}}>
+                    <ProductSection
+                        title='Revolving Credit Facility'
+                        productValid={rcfValid}
+                        items={rcfRates}
+                        interestRate={rCFInterestRate}
+                        onInterestChange={onRCFInterestChange}
+                    />
+                </span>
+                <span style={{width: '50%', marginLeft: 44}}>
+                    <ProductSection
+                        title='Business Loan'
+                        productValid={blValid}
+                        items={blRates}
+                        interestRate={bLInterestRate}
+                        onInterestChange={onBLInterestChange}
+                    />
+                </span>
+            </div>
+        );
+    }
+    const renderMain = () => {
+        return (
+            <>
+                {renderForm()}
+                {renderTables()}
+            </>
+        );
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <form>
-                    <div>
-                        <label>
-                            Amount requested
-                            <input
-                                type="number"
-                                value={amount}
-                                min={minAmount}
-                                max={maxAmount}
-                                onChange={onAmountChange}
-                            />
-                            (£)
-                        </label>
-                    </div>
-                    <div>
-                        Duration
-                        <input
-                            type="number"
-                            value={duration}
-                            min={minDuration}
-                            max={maxDuration}
-                            onChange={onDurationChange}
-                        />
-                        (months)
-                    </div>
-                </form>
-                <div style={{display: 'flex', padding: 44}}>
-                    <span style={{width: '50%'}}>
-                        <TableSection
-                            title='Revolving Credit Facility'
-                            items={rcfRates}
-                            interestRate={rCFInterestRate}
-                            onInterestChange={onRCFInterestChange}
-                        />
-                    </span>
-                    <span style={{width: '50%', marginLeft: 44}}>
-                        <TableSection
-                            title='Business Loan'
-                            items={blRates}
-                            interestRate={bLInterestRate}
-                            onInterestChange={onBLInterestChange}
-                        />
-                    </span>
-                </div>
+                {configLoaded
+                    ? renderMain()
+                    : <div>Loading...</div>
+                }
             </header>
         </div>
     );
 }
 
-const TableSection = (props) => {
-    const {items, interestRate} = props;
+const ProductSection = (props) => {
+    const {productValid, items, interestRate} = props;
 
     const onInterestRateChange = (ev) => {
         props.onInterestChange(ev.target.value);
     };
+
     return (
         <div style={{backgroundColor: '#444', padding: 22}}>
-            <form>
-                <label>
-                    Interest rate
-                    <input
-                        type="number"
-                        value={interestRate}
-                        onChange={onInterestRateChange}
-                    />
-                    (%)
-                </label>
-            </form>
-            <Table items={items}></Table>
             <h2>{props.title}</h2>
+            {productValid &&
+                <>
+                    <form>
+                        <label>
+                            Interest rate
+                            <input
+                                type="number"
+                                value={interestRate}
+                                onChange={onInterestRateChange}
+                            />
+                            (%)
+                        </label>
+                    </form>
+                    <Table items={items}></Table>
+                </>
+            }
+            {!productValid &&
+                <div>
+                    (This product is not available for your combination of amount and/or duration)
+                </div>
+            }
         </div>
     )
 }
