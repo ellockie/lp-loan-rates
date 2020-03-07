dae24fab478ac7e957106524d81a36696a8e7969
Lukasz Przewlocki <luxxart@gmail.com>
2020-03-07 12:50:47 +0000

    semantic ui added in the background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 import axios from 'axios';
 import './App.css';
 
@@ -205,7 +206,8 @@ const ProductSection = (props) => {
                             (%)
                         </label>
                     </form>
-                    <Table items={items}></Table>
+                    <InstallmentsTable items={items}></InstallmentsTable>
+                    {/* <div><TableExamplePagination/></div> */}
                 </>
             }
             {!productValid &&
@@ -221,7 +223,7 @@ const roundMoney = (amount) => {
     return new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(amount);
 }
 
-const Table = (props) => {
+const InstallmentsTable = (props) => {
 
     const getColumnTotal = (fieldName) => {
         return props.items.reduce((sum, item) => sum + item[fieldName], 0)
@@ -270,5 +272,56 @@ const Table = (props) => {
         </>
     )
 }
+
+const TableExamplePagination = () => (
+    <Table celled>
+        <Table.Header>
+            <Table.Row>
+                <Table.HeaderCell>Header</Table.HeaderCell>
+                <Table.HeaderCell>Header</Table.HeaderCell>
+                <Table.HeaderCell>Header</Table.HeaderCell>
+            </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+            <Table.Row>
+                <Table.Cell>
+                    <Label ribbon>First</Label>
+                </Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+            </Table.Row>
+        </Table.Body>
+
+        <Table.Footer>
+            <Table.Row>
+                <Table.HeaderCell colSpan='3'>
+                    <Menu floated='right' pagination>
+                        <Menu.Item as='a' icon>
+                            <Icon name='chevron left' />
+                        </Menu.Item>
+                        <Menu.Item as='a'>1</Menu.Item>
+                        <Menu.Item as='a'>2</Menu.Item>
+                        <Menu.Item as='a'>3</Menu.Item>
+                        <Menu.Item as='a'>4</Menu.Item>
+                        <Menu.Item as='a' icon>
+                            <Icon name='chevron right' />
+                        </Menu.Item>
+                    </Menu>
+                </Table.HeaderCell>
+            </Table.Row>
+        </Table.Footer>
+    </Table>
+)
 
 export default App;
