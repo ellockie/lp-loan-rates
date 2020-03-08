6bfa5d7f0c2e2f97e115e98c9d7e9bf34f6a2a22
Lukasz Przewlocki <luxxart@gmail.com>
2020-03-08 01:39:00 +0000

    split into modules; fixing linter errors

---
+++ src/components/ProductSection.tsx
@@ -0,0 +1,49 @@
+import React, { ChangeEvent } from 'react';
+
+import { RepaymentsTable, IRepayment } from './RepaymentsTable'
+
+
+interface IProductSectionProps {
+    title: string,
+    productValid: boolean,
+    repayments: IRepayment[],
+    interestRate: number,
+    onInterestChange(newRate: number): void
+}
+
+
+export const ProductSection = (props: IProductSectionProps): JSX.Element => {
+    const { productValid, repayments, interestRate } = props;
+
+    const onInterestRateChange = (ev: ChangeEvent<HTMLInputElement>): void => {
+        props.onInterestChange(parseInt(ev.target.value));
+    };
+
+    return (
+        <div style={{ backgroundColor: '#444', padding: 22 }}>
+            <h2>{props.title}</h2>
+            {productValid &&
+                <>
+                    <form>
+                        <label>
+                            Interest rate
+                            <input
+                                type="number"
+                                value={interestRate}
+                                onChange={onInterestRateChange}
+                            />
+                            (%)
+                        </label>
+                    </form>
+                    <RepaymentsTable repayments={repayments}></RepaymentsTable>
+                    {/* <div><TableExamplePagination/></div> */}
+                </>
+            }
+            {!productValid &&
+                <div>
+                    (This product is not available for your combination of amount and/or duration)
+                </div>
+            }
+        </div>
+    )
+}
---
+++ src/components/RepaymentsTable.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+
+export interface IRepayment {
+    dateString: string
+    principal: number,
+    interest: number,
+    total: number
+}
+
+interface IRepaymentsTableProps {
+    repayments: IRepayment[]
+}
+
+type RepaymentField = 'principal' | 'interest' | 'total';
+
+
+export const RepaymentsTable = (props: IRepaymentsTableProps): JSX.Element => {
+
+    const formatCurrency = (amount: number): string => {
+        return new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(amount);
+    }
+
+    const getColumnTotal = (fieldName: RepaymentField): number => {
+        return props.repayments
+            .reduce((sum: number, repayment: IRepayment) => sum + repayment[fieldName], 0)
+    }
+
+    const getPrincipalTotal = (): string => {
+        return formatCurrency(getColumnTotal('principal'));
+    }
+    const getInterestTotal = (): string => {
+        return formatCurrency(getColumnTotal('interest'));
+    }
+    const getTotal = (): string => {
+        return formatCurrency(getColumnTotal('total'))
+    }
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Principal</th>
+                        <th>Interest</th>
+                        <th>Total Repayment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.repayments.map((repayment: IRepayment) =>
+                        (<tr key={repayment.dateString}>
+                            <td>{repayment.dateString}</td>
+                            <td>{formatCurrency(repayment.principal)}</td>
+                            <td>{formatCurrency(repayment.interest)}</td>
+                            <td>{formatCurrency(repayment.total)}</td>
+                        </tr>)
+                    )}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Total</th>
+                        <th>{getPrincipalTotal()}</th>
+                        <th>{getInterestTotal()}</th>
+                        <th>{getTotal()}</th>
+                    </tr>
+                </tfoot>
+            </table>
+        </>
+    )
+}
